15b281b91f2cfbb76459ef3f30a3f77c71611cc4
gui <guilhermev2huehue@gmail.com>
2024-10-25 19:35:03 -0300

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let  listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port.");
-    
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+
     run(listener)?.await
 }
